af300368c7647011819bf61614f61f6335bf78e3
SultanOfSauce <massaria44@gmail.com>
2022-11-20 15:59:55 +0100

    commit

---
+++ 1-1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const WindowSize = 3
+
+func main() {
+
+	vals := GatherValues("input.txt")
+
+	windowedValues := make([]int, len(vals)-WindowSize)
+
+	counter := 0
+
+	for i := range windowedValues {
+		sum := 0
+		for j := 0; j < WindowSize; j++ {
+			sum += vals[i+j]
+		}
+		windowedValues[i] = sum
+	}
+
+	for i := 1; i < len(windowedValues); i++ {
+		if windowedValues[i]-windowedValues[i-1] > 0 {
+			counter++
+		}
+		/*
+			isDecreased := vals[i] - vals[i-1] > 0
+			decreases[i] = vals[i] - vals[i-1] > 0
+		*/
+	}
+
+	fmt.Println(counter)
+}
+
+func GatherValues(filename string) (vals []int) {
+	bytes, err := os.ReadFile(filename)
+	content := string(bytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content = strings.TrimSpace(content)
+	strs := strings.Split(string(content), "\n")
+
+	vals = make([]int, len(strs))
+
+	for i := range vals {
+		vals[i], _ = strconv.Atoi(strs[i])
+	}
+
+	return
+}
